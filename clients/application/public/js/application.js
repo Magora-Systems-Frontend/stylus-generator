3c5b2facdff5a8130e68a5896f6e86f763b0dc88
Pavel Akulov <akulov@magora-systems.ru>
2016-03-17 11:29:56 +0600

    [*] remove unnecessary files from git

--- clients/application/public/js/application.js
+++ clients/application/public/js/application.js
@@ -1,13 +1,3 @@
-Array.prototype.unique = function() {
-    var a = this.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j, 1);
-        }
-    }
-    return a;
-};
 (function (angular) {
     'use strict';
 
@@ -2770,3 +2760,14 @@ module
     }
 
 })(angular);
+
+Array.prototype.unique = function() {
+    var a = this.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j, 1);
+        }
+    }
+    return a;
+};
